78ca204da20c1fe318c4c23a1420b9b533658b81
ShadowTaze <mattdavep@gmail.com>
2017-02-18 12:34:41 -0500

    Added network functionality.  Note that addresses are broken.

--- Network/PiClient.java
+++ Network/PiClient.java
@@ -1,7 +1,13 @@
-import java.net.Socket;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.net.Inet4Address;
 
 public class PiClient {
-	private Socket server;
+	private DatagramSocket connection;
+	private int localPort;
+	private int bufferLength;
+	private byte[] buffer;
+
 	private String serverIP;
 	private int serverPort;
 
@@ -10,13 +16,27 @@ public class PiClient {
 	}
 
 	public PiClient(Boolean test) {
-		this.serverIP = test ? "127.0.0.1": "10.0.0.14";
+		this.serverIP = test ? "127.0.0.1": "10.0.0.18";
+		//this.Inet4Address = new Inet4Address(IP);
 		this.serverPort = serverPort;
+		this.bufferLength = 576;
+		this.buffer = new byte[this.bufferLength];
 	}
 
-	private Socket connectServer() {
-		Socket  connection = new Socket();
-		
+	private DatagramSocket connectServer() {
+		DatagramPacket connectionPacket = new DatagramPacket(this.buffer, this.bufferLength);
+
+		this.connection = new DatagramSocket(this.localPort);
+		this.connection.connect(this.serverIP, this.serverPort);
+
+		this.connection.receive(connectionPacket);
+
+		this.connection.disconnect();
+		this.connection.close();
 		return connection;
+	}
+
+	private void setPort(int newPort) {
+		this.serverPort = newPort;
 	}
 }
