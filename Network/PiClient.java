520d5eb65169c785a7a3f6118ac9b89be3afd46f
ShadowTaze <mattdavep@gmail.com>
2017-02-20 22:54:57 -0500

    Please use a branch if you are modifying a file you did not create.  Thanks!

--- Network/PiClient.java
+++ Network/PiClient.java
@@ -2,48 +2,38 @@ import java.net.DatagramSocket;
 import java.net.DatagramPacket;
 import java.net.Inet4Address;
 
-public class JSONReceiver {
-	private DatagramSocket socket;
+public class PiClient {
+	private DatagramSocket connection;
 	private int localPort;
 	private int bufferLength;
 	private byte[] buffer;
-	
+
 	private String serverIP;
 	private int serverPort;
-	
-	
 
-	public JSONReceiver() {
-		this(13375);
+	public PiClient() {
+		this(false);
 	}
 
-	public JSONReceiver(int port){
-		this("10.0.0.18", port);
-	}
-
-	public PiClient(String serverIP, int serverPort) {
-		//this.serverIP = test ? "127.0.0.1": "10.0.0.18";
+	public PiClient(Boolean test) {
+		this.serverIP = test ? "127.0.0.1": "10.0.0.18";
 		//this.Inet4Address = new Inet4Address(IP);
-		this.serverIP = serverIP;
 		this.serverPort = serverPort;
 		this.bufferLength = 576;
 		this.buffer = new byte[this.bufferLength];
-		socket = new DatagramSocket(ServerPort);
-		
 	}
 
-	
+	private DatagramSocket connectServer() {
+		DatagramPacket connectionPacket = new DatagramPacket(this.buffer, this.bufferLength);
 
+		this.connection = new DatagramSocket(this.localPort);
+		this.connection.connect(this.serverIP, this.serverPort);
 
-	private DatagramPacket connectServer() {
-		DatagramPacket packet = new DatagramPacket(this.buffer, this.bufferLength);
+		this.connection.receive(connectionPacket);
 
-
-		this.socket.receive(packet);
-
-		this.socket.disconnect();
-		this.socket.close();
-		return packet;
+		this.connection.disconnect();
+		this.connection.close();
+		return connection;
 	}
 
 	private void setPort(int newPort) {
