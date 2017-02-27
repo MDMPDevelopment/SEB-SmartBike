26cdfd0984dd59cb3d995cec91cd0dde13facbcf
ShadowTaze <mattdavep@gmail.com>
2017-02-26 19:07:54 -0500

    Updated PiClient.java

--- Network/PiClient.java
+++ Network/PiClient.java
@@ -1,42 +1,15 @@
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
 import java.net.Inet4Address;
+import JSONReceiver;
+import DatabaseManager;
 
 public class PiClient {
-	private DatagramSocket connection;
-	private int localPort;
-	private int bufferLength;
-	private byte[] buffer;
-
-	private String serverIP;
-	private int serverPort;
+	private JSONReceiver rcv;
+	private DatabaseManager db;
 
 	public PiClient() {
-		this(false);
-	}
-
-	public PiClient(Boolean test) {
-		this.serverIP = test ? "127.0.0.1": "10.0.0.18";
-		//this.Inet4Address = new Inet4Address(IP);
-		this.serverPort = serverPort;
-		this.bufferLength = 576;
-		this.buffer = new byte[this.bufferLength];
-	}
-
-	private DatagramSocket connectServer() {
-		DatagramPacket connectionPacket = new DatagramPacket(this.buffer, this.bufferLength);
-
-		this.connection = new DatagramSocket(this.localPort);
-		this.connection.connect(this.serverIP, this.serverPort);
-
-		this.connection.receive(connectionPacket);
-
-		this.connection.disconnect();
-		this.connection.close();
-		return connection;
-	}
-
-	private void setPort(int newPort) {
-		this.serverPort = newPort;
+		rcv = new JSONReceiver();
+		db = new DatabaseManager();
 	}
 }
