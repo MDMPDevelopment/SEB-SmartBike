c03b36706de45d5bf62178339f90be7bfef32d2c
shanecorrigan <shane.corrigan@outlook.com>
2017-03-12 14:49:58 -0400

    Add files via upload

--- HandleBarButtons/buttonHandler.py
+++ HandleBarButtons/buttonHandler.py
@@ -1,28 +1,34 @@
 import RPi.GPIO as GPIO
 import socket, sys, time
 
+#GPIO Setup
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(26, GPIO.IN) #right input
 GPIO.setup(24, GPIO.IN) #left input
 
+#UDP setup
 host = sys.argv[1]
 textport = sys.argv[2]
- 
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 port = int(textport)
 server_address = (host, port)
 
+rightlight = 0
+leftlight = 0 
 while 1:
-    data = ""
-    if GPIO.input(26):
-        data = "turnR:1"
+    if GPIO.input(26): #wait for right button press
+        rightlight = not rightlight #change light state
+        #send packet
+		data = "turnR:%d" %rightlight
         s.sendto(data.encode('utf-8'), server_address)
         while GPIO.input(26):
             time.sleep(0.5)
-    if GPIO.input(24):
-        data = "turnL:1"
+    if GPIO.input(24): # wait for left button
+        leftlight = not leftlight #change light state
+        #send packet
+		data = "turnL:%d" %leftlight
         s.sendto(data.encode('utf-8'), server_address)
         while GPIO.input(24):
             time.sleep(0.5)
-
 s.shutdown(1)
---
+++ HandleBarButtons/speedHandler.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import socket, sys, time
+#GPIO setup
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(22, GPIO.IN) #speed
+
+#UDP setup
+host = sys.argv[1]
+textport = sys.argv[2]
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = int(textport)
+server_address = (host, port)
+
+#data for calculations
+radius = 0.29 #radius of bike wheel
+pi = 3.14159 
+count = 0
+rotations = 5 #number of rotations
+wait = 0.1 #wait used for debouncing
+start = time.time() #timer
+
+while 1:  
+  if not GPIO.input(22):
+    time.sleep(wait) # wait so only one reading occurs
+    if count == rotations: #enter if max rotation
+      t = time.time() - start - wait #time for 5 rotations
+      speed = (2*pi*radius*rotations)/t #speed calc
+      #send packet
+      data = "speed:%f" %speed
+      s.sendto(data.encode('utf-8'), server_address)
+      #reset count and time
+      start = time.time()
+      count = 0
+    else:
+      count = count + 1
+s.shutdown(1)
+      
