4409f4aa67cd21222045f8fc7cf935a4777c519a
Ben Earle <ben.earle@live.ca>
2017-03-14 14:12:20 -0400

    Small app changes

--- Android/SEB/app/src/main/java/safeelectronicbike/seb/MainActivity.java
+++ Android/SEB/app/src/main/java/safeelectronicbike/seb/MainActivity.java
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startRide(View view){
-        Toast.makeText(this, "Starting ride tracking...", Toast.LENGTH_SHORT).show();\
+        Toast.makeText(this, "Starting ride tracking...", Toast.LENGTH_SHORT).show();
 
     }
 
