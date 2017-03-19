d707cba810a657d7a0b3f1c63ec14f04585ddfdf
Ben Earle <ben.earle@live.ca>
2017-03-19 15:23:40 -0400

    Added system state activity

--- Android/SEBapp/app/src/main/java/com/example/benearle/sebapp/StateActivity.java
+++ Android/SEBapp/app/src/main/java/com/example/benearle/sebapp/StateActivity.java
@@ -4,15 +4,20 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import android.view.*;
+import android.widget.TextView;
+
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class StateActivity extends AppCompatActivity {
-
+    private final int PACKETSIZE = 500;
     private DatagramSocket socket;
     private InetAddress host;
     private int port;
+    private int speed;
+    TextView textView;
 
     private void initConnection() throws Exception {
         host = InetAddress.getByName("10.0.0.13");
@@ -20,16 +25,42 @@ public class StateActivity extends AppCompatActivity {
         socket = new DatagramSocket();
     }
 
+    private void updateInfo() throws Exception{
+        byte[] data = "getState:13375".getBytes();
+        socket.send(new DatagramPacket(data, data.length, host, port));
+        DatagramSocket s = new DatagramSocket(13375);
+        DatagramPacket packet = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE );
+        s.receive(packet);
+
+        String state = new String(packet.getData()).trim();
+        String[] pairs = state.split(": ");
+        for(int i = 0; i < pairs.length; i+=2){
+            if(pairs[i] == "Speed") speed = Integer.parseInt(pairs[i+1]);
+        }
+        textView.setText("Speed: " + speed);
+    }
+
+
+    public void refresh (View view) {
+        try{
+            updateInfo();
+        } catch (Exception e){
+            System.out.println("Could not refresh.");
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        speed = 0;
         setContentView(R.layout.activity_state);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        byte[] data = "getState:".getBytes();
+        textView = (TextView) findViewById(R.id.speedView);
+
         try {
             initConnection();
-            socket.send(new DatagramPacket(data, data.length, host, port));
+            updateInfo();
         } catch (Exception e){
             System.out.println("ERROR");
         }
