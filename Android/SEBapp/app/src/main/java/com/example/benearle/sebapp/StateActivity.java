db8ee4a383e1b10d7c581d556e1adf9bf87a09a9
BenEarle <ben.earle@live.ca>
2017-03-20 21:35:29 -0400

    added RequestState + interface

--- Android/SEBapp/app/src/main/java/com/example/benearle/sebapp/StateActivity.java
+++ Android/SEBapp/app/src/main/java/com/example/benearle/sebapp/StateActivity.java
@@ -12,32 +12,14 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class StateActivity extends AppCompatActivity {
-    private final int PACKETSIZE = 500;
-    private final String ip = "192.168.145.130";
-    private DatagramSocket socket;
-    private InetAddress host;
-    private int port;
     private int speed;
     TextView textView;
-
-    private void initConnection() throws Exception {
-        host = InetAddress.getByName(ip);
-        port = 13375;
-        socket = new DatagramSocket();
-    }
+    RequestState rs;
 
     private void updateInfo() throws Exception{
-        byte[] data = "getState:13375".getBytes();
-        socket.send(new DatagramPacket(data, data.length, host, port));
-
-        DatagramSocket s = new DatagramSocket(13375);
-        DatagramPacket packet = new DatagramPacket(new byte[PACKETSIZE], PACKETSIZE );
-        s.receive(packet);
-
-        String state = new String(packet.getData()).trim();
-        String[] pairs = state.split(": ");
-        for(int i = 0; i < pairs.length; i+=2){
-            if(pairs[i] == "Speed") speed = Integer.parseInt(pairs[i+1]);
+        HashMap<String, String> state = new HashMap<>();
+        for (String key : state.keySet()){
+            if(key == "Speed") speed = Integer.parseInt(state.get(key));
         }
         textView.setText("Speed: " + speed);
     }
@@ -59,17 +41,8 @@ public class StateActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         textView = (TextView) findViewById(R.id.speedView);
-        try {
-            initConnection();
-        } catch (Exception e){
-            System.out.println("ERROR in initConnection: " + e.getMessage());
-        }
-
-        try {
-            updateInfo();
-        } catch (Exception e){
-            System.out.println("ERROR in updateInfo: " + e.getMessage());
-        }
+        rs = new RequestState();
+        updateInfo();
 
     }
 
