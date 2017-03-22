cfb34c3bb0094d3e40bcec18c223884e98f67ad3
BenEarle <ben.earle@live.ca>
2017-03-21 23:16:10 -0400

    Max speed and radius added to server

--- Server/Server.java
+++ Server/Server.java
@@ -9,7 +9,8 @@ public class Server {
 	private DatagramSocket socket;
 	private String serverIP;
 	private int serverPort;
-
+	private int maxSpeed;
+	private boolean speeding;
 
 	public Server() throws Exception {
 		this(13375);
@@ -28,6 +29,8 @@ public class Server {
 		this.serverPort = serverPort;
 		this.DbM = DbM;
 		this.running = false;
+		speeding = false;
+		maxSpeed = 999;
 		socket = new DatagramSocket(serverPort);
 	}
 
@@ -46,25 +49,43 @@ public class Server {
 		String[] pairs = s.split(":");
 		if(pairs.length == 2 ){
 			switch (pairs[0]){
-				case "GPS"	:	DbM.addMeasurement(pairs[0], pairs[1]);
-								DbM.setSystemState(pairs[0], pairs[1]);
+				case "GPS"	:		DbM.addMeasurement(pairs[0], pairs[1]);
+									DbM.setSystemState(pairs[0], pairs[1]);
 								break;
-				case "newRide": DbM.newRide();
-							break;
-				case "brake":	DbM.setSystemState(pairs[0],pairs[1]);
-							break;
-				case "turnL": 	DbM.setSystemState(pairs[0],pairs[1]);
-							break;
-				case "getState":sendState(packet.getAddress(), pairs[1]);
-							break;
-				case "turnR":	DbM.setSystemState(pairs[0],pairs[1]);
+				case "setRadius":	int rad = Integer.parseInt(pairs[1]);
+									System.out.println("rad: " + rad);
+									//assert(rad!=null);	
+									//DbM.setRad()
+				case "setMaxSpeed": maxSpeed = Integer.parseInt(pairs[1]);
 								break;
-				default:		DbM.addMeasurement(pairs[0], pairs[1]);
-								DbM.setSystemState(pairs[0], pairs[1]);
+				case "newRide": 	DbM.newRide();
+								break;
+				case "brake":		DbM.setSystemState(pairs[0],pairs[1]);
+								break;
+				case "turnL": 		DbM.setSystemState(pairs[0],pairs[1]);
+								break;
+				case "getState":	sendState(packet.getAddress(), pairs[1]);
+								break;
+				case "turnR":		DbM.setSystemState(pairs[0],pairs[1]);
+								break;
+				case "Speed":		DbM.addMeasurement(pairs[0], pairs[1]);
+									DbM.setSystemState(pairs[0], pairs[1]);
+									if(Integer.parseInt(pairs[1]) > maxSpeed & !speeding){
+										speeding = true;
+										alertRider();
+									}
+									if(speeding & Integer.parseInt(pairs[1]) < maxSpeed)
+										speeding = false;
+								break;
+				default:			System.out.println("Unknown request: " + s);
 								break;
 			}
 			//System.out.println("Type: " + pairs[0] + "\nValue: " + pairs[1]);
 		}
+	}
+
+	public void alertRider(){
+		System.out.println("TOO FAST!");
 	}
 
 	public void startReceiving() throws Exception {
