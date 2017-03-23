993621f4b7f19f93cd6f33ef104570a66c696b8f
shanecorrigan <shane.corrigan@outlook.com>
2017-03-23 10:14:20 -0400

    Add files via upload

---
+++ HandleBar/speedingLight.py
@@ -0,0 +1,30 @@
+import socket, sys, time
+import RPi.GPIO as GPIO
+
+textport = sys.argv[1]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+port = int(textport)
+server_address = ('10.0.0.13', port)
+s.bind(server_address)
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(19, GPIO.OUT)
+
+string1 = "slowRider:1"
+string2 = "slowRider:0"
+while True:
+
+    print ("Waiting to receive on port %d : press Ctrl-C or Ctrl-Break to stop " % port)
+
+    buf, address = s.recvfrom(13375)
+    if not len(buf):
+        break
+    if(buf.compare(string1)):
+		GPIO.output(19,1)
+		print("speeding")
+	if(buf.compare(string2)):
+		GPIO.output(19,0)
+		print("not speeding")
+	
+s.shutdown(1)
