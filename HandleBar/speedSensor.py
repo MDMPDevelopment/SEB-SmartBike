ef3103bd33fd6fc4c70246dfcd3fc3d6d5aff8f2
Ben Earle <ben.earle@live.ca>
2017-03-25 13:53:40 -0400

    Update speedSensor.py
    speed is 21

--- HandleBar/speedSensor.py
+++ HandleBar/speedSensor.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 
 class speedSensor(object):
 	#Initialize the GPIO.
-	def __init__(self, port=22):
+	def __init__(self, port=21):
 		self.port = port
 		GPIO.setmode(GPIO.BOARD)
 	`	GPIO.setup(port, GPIO.IN) #speed
 
-	#Returns the value of the sensor connected to port 22.
+	#Returns the value of the sensor connected to port 21.
 	def sensorVal():
 		return GPIO.input(self.port)
